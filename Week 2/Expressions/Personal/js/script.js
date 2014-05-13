f6665a9616c4243ea6a5014a3b2cf905de4492df
Jonathan Reynolds <Jonathan@Jonathans-MacBook-Pro.local>
2014-05-13 15:48:34 -0400

    First Update to Personal Calculator
    Wrote down the expected results of the first calculator which will be
    the Personal Calculator. This Calculator will be a Restaurant Tip
    Calculator.

--- Week 2/Expressions/Personal/js/script.js
+++ Week 2/Expressions/Personal/js/script.js
@@ -1,1 +1,4 @@
-alert("This is in the js file");
+//For My Personal Assignment I decided to create a Tip Calculator. 
+//The tip calculator will calculate how much the EndUser should tip the waiter/waitress.
+//The calculator will base this information off of 4 percentages.
+//3 of them being 15% | 20% | 25% and the fourth being the User Input Amount
