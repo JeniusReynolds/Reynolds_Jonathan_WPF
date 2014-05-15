0036a689f269ed896188dc52606f425b48e447b4
Jonathan Reynolds <Jonathan@Jonathans-MacBook-Pro.local>
2014-05-15 12:54:51 -0400

    Updated JS Camel Casing
    Updated JS Camel Casing as well as updated comments section for both
    Personal and Wacky JavaScript Files.

--- Week 2/Expressions/Personal/js/script.js
+++ Week 2/Expressions/Personal/js/script.js
@@ -1,38 +1,44 @@
-//For My Personal Assignment I decided to create a Tip Calculator. 
-//The tip calculator will calculate how much the EndUser should tip the waiter/waitress.
-//The calculator will base this information off of 4 percentages.
-//3 of them being 15% | 20% | 25% and the fourth being the User Input Amount
+/* Jonathan Reynolds
+ * WPF1405-01
+ * Personal Calculator
+ * May 15, 2014
+
+For My Personal Assignment I decided to create a Tip Calculator. 
+The tip calculator will calculate how much the EndUser should tip the waiter/waitress.
+The calculator will base this information off of 4 percentages.
+3 of them being 15% | 20% | 25% and the fourth being the User Input Amount
+*/
 
 //Declaration of Constants and Variables
 
-const Fifteen = .15;
-const Twenty = .20;
-const TwentyFive = .25;
-var TotalDinnerCost;
-var DesiredTip;
-var Welcome = "Hello, Welcome to the Restaurant Tip Calculator";
-var TotalBill;
+const fifteen = .15;
+const twenty = .20;
+const twentyFive = .25;
+var totalMealCost;
+var desiredTip;
+var welcome = "Hello, welcome to the Restaurant Tip Calculator";
+var totalBill;
 
 //Adding the inital prompts/alerts
 
-alert(Welcome);
-DesiredTip = prompt("Please enter the desired tip percentage that you would like to tip the waiter/waitress. No percentage sign needed. Thanks");
-TotalDinnerCost = prompt("How much is the total for your meal? No Dollar Sign Needed. Thanks");
+alert(welcome);
+desiredTip = prompt("Please enter the desired tip percentage that you would like to tip the waiter/waitress. No percentage sign needed. Thanks");
+totalMealCost = prompt("How much is the total for your meal? No Dollar Sign Needed. Thanks");
 
 //Calculations needed after initial prompts
 
-var Tip15 = TotalDinnerCost * Fifteen;
-var Tip20 = TotalDinnerCost * Twenty;
-var Tip25 = TotalDinnerCost * TwentyFive;
-var TipUser = TotalDinnerCost * (DesiredTip/100);
+var tip15 = totalMealCost * fifteen;
+var tip20 = totalMealCost * twenty;
+var tip25 = totalMealCost * twentyFive;
+var tipUser = totalMealCost * (desiredTip/100);
 
-//Creating a an Array to calculate the Final Bill for each Tip Amount. The final bill calculation is TotalDinnerCost + The Various Tip Amounts
+//Creating a an Array to calculate the Final Bill for each Tip Amount. The final bill calculation is totalMealCost + The Various Tip Amounts
 
-TotalBill = [TipUser+parseFloat(TotalDinnerCost) , Tip15+parseFloat(TotalDinnerCost) , Tip20+parseFloat(TotalDinnerCost) , Tip25+parseFloat(TotalDinnerCost)];
+totalBill = [tipUser+parseFloat(totalMealCost) , tip15+parseFloat(totalMealCost) , tip20+parseFloat(totalMealCost) , tip25+parseFloat(totalMealCost)];
 
 // This will be the final promp as well as console log for the calculator.
 
-alert("Your desired tip percentage is "+DesiredTip+"%. Your total meal cost is $"+TotalDinnerCost+". This makes your tip amount $"+TipUser+". If you decide to tip this amount your total bill will be $"+TotalBill[0]+". If you instead desire to tip 15%, 20%, or 25% the tip amounts would be $"+Tip15+", $"+Tip20+", or $"+ Tip25+". Which would respectfully make your total bill costs $"+TotalBill[1]+", $"+TotalBill[2]+", or $"+ TotalBill[3]+".");
+alert("Your desired tip percentage is "+desiredTip+"%. Your total meal cost is $"+totalMealCost+". This makes your tip amount $"+tipUser+". If you decide to tip this amount your total bill will be $"+totalBill[0]+". If you instead desire to tip 15%, 20%, or 25% the tip amounts would be $"+tip15+", $"+tip20+", or $"+ tip25+". Which would respectfully make your total bill costs $"+totalBill[1]+", $"+totalBill[2]+", or $"+ totalBill[3]+".");
 
 
-console.log("Your desired tip percentage is "+DesiredTip+"%. Your total meal cost is $"+TotalDinnerCost+". This makes your tip amount $"+TipUser+". If you decide to tip this amount your total bill will be $"+TotalBill[0]+". If you instead desire to tip 15%, 20%, or 25% the tip amounts would be $"+Tip15+", $"+Tip20+", or $"+ Tip25+". Which would respectfully make your total bill costs $"+TotalBill[1]+", $"+TotalBill[2]+", or $"+ TotalBill[3]+".");
+console.log("Your desired tip percentage is "+desiredTip+"%. Your total meal cost is $"+totalMealCost+". This makes your tip amount $"+tipUser+". If you decide to tip this amount your total bill will be $"+totalBill[0]+". If you instead desire to tip 15%, 20%, or 25% the tip amounts would be $"+tip15+", $"+tip20+", or $"+ tip25+". Which would respectfully make your total bill costs $"+totalBill[1]+", $"+totalBill[2]+", or $"+ totalBill[3]+".");
--- Week 2/Expressions/Wacky/js/script.js
+++ Week 2/Expressions/Wacky/js/script.js
@@ -1,10 +1,30 @@
-//For My Wacky Calculator, I decided to do a calculator that isn't necessarily "Wacky" in nature.
-//I chose to do something that would require quite a few expressions as well as some of the other functions we have learned thus far.
-//I am choosing to create a BMI[Body Mass Index] calculator.
-//I visited www.wikihow.com to find the calculation for Body Mass Index. The Steps are:
-//Obtain Weight
-//Obtain Height in Inches -- For this step I will get height in Feet and Inches from the user then convert the values to inches for calculations
-//Square the height in inches
-//Divide Weight by the Squared number
-//Multiply that answer by a conversion factor of 703. That number is the BMI
-//Once BMI is obtained I will create an alert informing the user of BMI as well as what the ranges should be.
+/* Jonathan Reynolds
+ * WPF1405-01
+ * Wacky Calculator
+ * May 15, 2014
+ 
+For My Wacky Calculator, I decided to do a calculator that isn't necessarily "Wacky" in nature.
+I chose to do something that would require quite a few expressions as well as some of the other functions we have learned thus far.
+I am choosing to create a BMI[Body Mass Index] calculator.
+I visited www.wikihow.com to find the calculation for Body Mass Index. The Steps are:
+Obtain Weight
+Obtain Height in Inches -- For this step I will get height in Feet and Inches from the user then convert the values to inches for calculations
+Square the height in inches
+Divide Weight by the Squared number
+Multiply that answer by a conversion factor of 703. That number is the BMI
+Once BMI is obtained I will create an alert informing the user of BMI as well as what the ranges should be.
+*/
+
+
+
+//Going to create Constant and Variables. Some more may be needed after.
+
+const ConversionFactor = 703;
+var weight;
+var heightInInches;
+var heightFeet;
+var heightInches;
+var heightSquared;
+var bmiCalc1;
+var bmiFinal;
+
