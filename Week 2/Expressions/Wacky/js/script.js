542a12359efb74a315136d91e2469b64ae5be5c3
Jonathan Reynolds <Jonathan@Jonathans-MacBook-Pro.local>
2014-05-15 12:07:22 -0400

    Wacky Calculator
    I have decided to do a BMI calculator for my Wacky Calculator. For this
    Commit I have updated the HTML file with the correct title. I have also
    updated the JavaScript file with notes in regards to what is supposed
    to happen.

--- Week 2/Expressions/Wacky/js/script.js
+++ Week 2/Expressions/Wacky/js/script.js
@@ -1,1 +1,10 @@
-alert("This is in the js file");
+//For My Wacky Calculator, I decided to do a calculator that isn't necessarily "Wacky" in nature.
+//I chose to do something that would require quite a few expressions as well as some of the other functions we have learned thus far.
+//I am choosing to create a BMI[Body Mass Index] calculator.
+//I visited www.wikihow.com to find the calculation for Body Mass Index. The Steps are:
+//Obtain Weight
+//Obtain Height in Inches -- For this step I will get height in Feet and Inches from the user then convert the values to inches for calculations
+//Square the height in inches
+//Divide Weight by the Squared number
+//Multiply that answer by a conversion factor of 703. That number is the BMI
+//Once BMI is obtained I will create an alert informing the user of BMI as well as what the ranges should be.
