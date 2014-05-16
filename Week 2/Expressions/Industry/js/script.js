480f24a79672d0adca708cf5191333e394d174a4
Jonathan Reynolds <Jonathan@Jonathans-MacBook-Pro.local>
2014-05-15 23:13:09 -0400

    Final Coding Update
    Tested and confirmed the coding for all 3 projects.

--- Week 2/Expressions/Industry/js/script.js
+++ Week 2/Expressions/Industry/js/script.js
@@ -21,7 +21,7 @@ const monthlyPayPeriods = 12;
 const semiMonthlyPayPeriods = 24;
 const annualHours = 2080;
 alert(welcome);
-userSalary=Number(prompt("Please enter your gross annual salary. No dollar sign is needed"));
+userSalary=Number(prompt("Please enter your gross annual salary. No commas or dollar signs are needed"));
 
 
 //Creating the calculations needed
@@ -42,8 +42,35 @@ semiMonthlyRate = userSalary/semiMonthlyPayPeriods;
 semiMonthlyRate = +semiMonthlyRate.toFixed(2);
 
 //Creating the Final Alert and Console Log
-console.log("Your annual salary is $"+userSalary+" which means your base rate is $"+hourlyRate+" an hour. Your monthly salary is $"+monthlyRate+" a month. Your semi-monthly rate is $"+semiMonthlyRate+". Your weekly and bi-weekly rates are $"+weeklyRate+ " and $"+biWeeklyRate+".")
+console.log("Your gross annual salary is $"+userSalary+". This means your base rate is $"+hourlyRate+" an hour, your monthly salary is $"+monthlyRate+", your semi-monthly rate is $"+semiMonthlyRate+", your weekly rate is $"+weeklyRate+ ", and your bi-weekly rate is $"+biWeeklyRate+".")
 
-alert("Your annual salary is $"+userSalary+" which means your base rate is $"+hourlyRate+" an hour. Your monthly salary is $"+monthlyRate+" a month. Your semi-monthly rate is $"+semiMonthlyRate+". Your weekly and bi-weekly rates are $"+weeklyRate+ " and $"+biWeeklyRate+".")
+alert("Your gross annual salary is $"+userSalary+". This means your base rate is $"+hourlyRate+" an hour, your monthly salary is $"+monthlyRate+", your semi-monthly rate is $"+semiMonthlyRate+", your weekly rate is $"+weeklyRate+ ", and your bi-weekly rate is $"+biWeeklyRate+".")
 
+
+//This portion is added for complexity
+
+desiredRaise = Number(prompt("If you were to ask for a raise based upon your current salary, what percentage increase would you like to see your salary go to?[No percentage sign is needed]"))
+
+//Calculations
+userSalaryR = userSalary * (1 + (desiredRaise/100))
+
+hourlyRateR = userSalaryR / annualHours;
+hourlyRateR = +hourlyRateR.toFixed(2);
+
+biWeeklyRateR = userSalaryR / biWeeklyPayPeriods;
+biWeeklyRateR = +biWeeklyRateR.toFixed(2);
+
+weeklyRateR = userSalaryR / weeklyPayPeriods;
+weeklyRateR = +weeklyRateR.toFixed(2);
+
+monthlyRateR = userSalaryR /monthlyPayPeriods;
+monthlyRateR = +monthlyRateR.toFixed(2);
+
+semiMonthlyRateR = userSalaryR/semiMonthlyPayPeriods;
+semiMonthlyRateR = +semiMonthlyRateR.toFixed(2);
+
+
+console.log("If you received a "+desiredRaise+"% raise, your new gross annual salary would be $"+userSalaryR+". This means your new base rate would be $"+hourlyRateR+" an hour, your monthly salary would be $"+monthlyRateR+", your semi-monthly rate would be $"+semiMonthlyRateR+", your weekly rate would be $"+weeklyRateR+ ", and your bi-weekly rate would be $"+biWeeklyRateR+".")
+
+alert("If you received a "+desiredRaise+"% raise, your new gross annual salary would be $"+userSalaryR+". This means your new base rate would be $"+hourlyRateR+" an hour, your monthly salary would be $"+monthlyRateR+", your semi-monthly rate would be $"+semiMonthlyRateR+", your weekly rate would be $"+weeklyRateR+ ", and your bi-weekly rate would be $"+biWeeklyRateR+".")
 
