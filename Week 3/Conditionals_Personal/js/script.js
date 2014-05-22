f5f58b547fed7a67b4976be69a2e5849987239bc
Jonathan Reynolds <Jonathan@Jonathans-MacBook-Pro.local>
2014-05-22 01:12:50 -0400

    Added the notes needed for my Personal Calculator
    Updated the Index HTML file as well as the JS file with the commented
    out notes needed.

--- Week 3/Conditionals_Personal/js/script.js
+++ Week 3/Conditionals_Personal/js/script.js
@@ -1,1 +1,8 @@
-alert("This is in the js file");
+/* Jonathan Reynolds
+ * WPF1405-01
+ * May 22, 2014
+ * 
+ * For my Personal Conditional exercise I have decided to create a simple calculator to ask the user if they have more shoes than me. If they have less shoes than me it will display the amount less that they have than me and vice versa if they have more
+ * 
+ */
+
