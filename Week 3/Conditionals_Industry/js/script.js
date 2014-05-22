adcf2bc90ec874d20d049b427bc70314f3c0e9eb
Jonathan Reynolds <Jonathan@Jonathans-MacBook-Pro.local>
2014-05-21 21:44:44 -0400

    Added 3 new folders Conditional assignments
    Added the initial comment notes for my industry conditional assignment.

---
+++ Week 3/Conditionals_Industry/js/script.js
@@ -0,0 +1,10 @@
+/* Jonathan Reynolds
+ * WPF1405-01
+ * Industry Calculator
+ * May 22, 2014
+ *
+ *
+ *For my industry portion I am going to create a "program" that will calculate a fictitous payroll tax. The tax percentage amount will be contingent tiered based upon amount. The calc is going to obtain the taxable amount from the user.
+ */
+
+
