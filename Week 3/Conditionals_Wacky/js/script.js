38dc65d767bcbb48b2977c5a7ec3c3eab38e8f2f
Jonathan Reynolds <Jonathan@Jonathans-MacBook-Pro.local>
2014-05-22 20:51:20 -0400

    Updated the coding
    Updated coding for my Wacky JS. I will be testing it once I add all of
    the explanation comments for the code that is occurring.

--- Week 3/Conditionals_Wacky/js/script.js
+++ Week 3/Conditionals_Wacky/js/script.js
@@ -11,14 +11,28 @@
 
 const mysteryNumber = 8;
 name = prompt("Hello, Welcome to the Mystery Number Game. To get started please provide your first name")
-firstGuess = Number(prompt("Thanks "+name+". I am thinking of a number 1 - 10. Guess what it is!"));
+firstGuess = Number(prompt("Thanks "+name+". I am thinking of a number 1 - 25. Guess what it is!"));
 
 //Adding If and If/Else statments to funnel the user into guessing the mystery number
 
-if(firstGuess === mysteryNumber){
-	alert("Lucky Guess! You guessed correctly. The mystery number was "+mysteryNumber+".")
-}else {
+if(firstGuess === mysteryNumber){//if the user guesses the number on the first try
+	alert("Lucky Guess "+name+"! You guessed correctly. The mystery number was "+mysteryNumber+".")
+}else { //If not, they are given another opportunity to guess the number after being given the numb
 	difference = mysteryNumber - firstGuess
 	posDiff = (difference > 0) ? difference : -difference
-	console.log (posDiff)
+	secondGuess = Number(prompt("Your first guess was off by "+posDiff+". Please make another guess."))
+	
+	if(secondGuess === mysteryNumber){
+		alert("Great job "+name+" you got it on your second guess")
+	}else{
+		difference = mysteryNumber - secondGuess
+		posDiff = (difference > 0) ? difference : -difference
+		thirdGuess = Number(prompt("Your first guess was off by "+posDiff+". Please make another guess."))
+		
+		if(thirdGuess === mysterNumber){
+			alert("You did a decent job "+name+". You figured it out on your 3rd try.")
+		}else{
+			alert("GAME OVER. You did not guess the number in the appropriate amount of tries.")
+		}
+	}
 }
