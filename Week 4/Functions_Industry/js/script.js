13b0d53fa8027133bf6891703b06a5fb4eb92a59
Jonathan Reynolds <Jonathan@Jonathans-MacBook-Pro.local>
2014-05-28 00:14:03 -0400

    Updated Industry Function
    Laid the groundwork down as far as what the calculator is supposed to
    do.

--- Week 4/Functions_Industry/js/script.js
+++ Week 4/Functions_Industry/js/script.js
@@ -1,1 +1,7 @@
-alert("This is in the js file");
+/*Jonathan Reynolds
+ * WPF1405-01
+ * Industry Function Assignment
+ * May 29, 2014
+ * 
+ * For my Industry function assignment I decided to do an Accruals calculator that will calculate how many accrual hours the user can expect to receive per year based upon an accrual calculator. Once that information is obtained it will also calculate what the value of those hours are based upon the employee's dollar per hour. The Accrual policy will be tiered on 3 levels continegent on how many years the employee has been with the company. The program wil obtain the user's tenure information and then make the appropriate calculations after.
+ */
