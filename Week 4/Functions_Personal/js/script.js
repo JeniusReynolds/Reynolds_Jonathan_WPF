4ee9bcc86f097ced920295d17f99ba71e5bfc1d1
Jonathan Reynolds <Jonathan@Jonathans-MacBook-Pro.local>
2014-05-29 01:02:41 -0400

    Updated HTML File and JS
    Updated HTML and JS File. Added initial notes as far as what I will be
    creating.

--- Week 4/Functions_Personal/js/script.js
+++ Week 4/Functions_Personal/js/script.js
@@ -1,1 +1,8 @@
-alert("This is in the js file");
+/*Jonathan Reynolds
+ * WPF1405-01
+ * Personal Calculator
+ * May 29, 2014
+ * 
+ * For my Personal calculator I am going to create a basic car payment calculator because a friend of mine just purchased a new vehicle
+ */
+
