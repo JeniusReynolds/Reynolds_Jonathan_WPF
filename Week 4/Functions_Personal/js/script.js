290c2351950b83b83c00294ba6186a756012cd91
Jonathan Reynolds <Jonathan@Jonathans-MacBook-Pro.local>
2014-05-29 01:45:30 -0400

    Added Initial Function
    Added the most pivotal function for the car payment loan. I will be
    testing it now to see if I have all of the operators in the right place.

--- Week 4/Functions_Personal/js/script.js
+++ Week 4/Functions_Personal/js/script.js
@@ -6,3 +6,12 @@
  * For my Personal calculator I am going to create a basic car payment calculator because a friend of mine just purchased a new vehicle
  */
 
+
+//I will be using an anonymous function to make the calculation for the car payment. I was able to obtain the formula for car payment from http://www.calcunation.com/calculators/business%20and%20finance/vehicle-payment.php. In order to make the formula work I had to investigate how to do the Power of in Javascript. I was able to find the answer via http://www.w3schools.com/jsref/jsref_pow.asp
+
+
+//This variable will meet the requirements for an Anonymous function as well as a function requiring 3 parameters.
+
+var carPayment = function(rate,principal,months){
+	var payment = (rate * principal) / 1 - (Math.pow((1+r),(-1*months)))
+}
