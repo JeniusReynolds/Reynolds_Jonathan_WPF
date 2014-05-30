9bd7756300c12c32ed386c2f3b5f4a751756277d
Jonathan Reynolds <Jonathan@Jonathans-MacBook-Pro.local>
2014-05-29 20:04:12 -0400

    Updated Wacky HTML and JS
    Updated my Wacky Calculator with the title on the HTML file and my
    initial notes as far as expected behavior for the calculator on the JS
    side.

--- Week 4/Functions_Wacky/js/script.js
+++ Week 4/Functions_Wacky/js/script.js
@@ -1,1 +1,7 @@
-alert("This is in the js file");
+/*Jonathan Reynolds
+ * WPF1405-01
+ * May 29, 2014
+ * 
+ * For my Wacky Calculator I have decided to create a simple Discount Calculator. Truthfully my other two Functional Calculators took a lot out of me in regards to creativity. I would say my Industry Calculator could also moonlight as my wacky calculator in terms of true complexity for a beginner. I digress. The Discount calculator will obtain from the user the initial price of an item and how much the discount is. From that information a function will take place to calculate the total discount as well as provide the final price after discount including tax
+ */
+
